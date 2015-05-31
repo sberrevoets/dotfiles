f208b21bb78eca903cff7d42c631f420f6109469
Scott Berrevoets <s.berrevoets@me.com>
2015-05-31 15:43:03 -0700

    Add OS X bootstrap

---
+++ osx/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Bootstrap OS X configuration
+
+if ! which brew &> /dev/null; then
+  echo "Installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap Homebrew/brewdler
+brew update
+brew bundle --file=Brewfile
