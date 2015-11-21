42acb4b84599b645066c0c71d5e87418be7c8999
Scott Berrevoets <s.berrevoets@me.com>
2015-11-21 00:50:24 -0800

    Add more OS X defaults

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -1,4 +1,4 @@
-### OS X ###
+### OS X
 
 # Enable dark UI
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
@@ -10,7 +10,35 @@ defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 0
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.twoFingerFromRightEdgeSwipeGesture -int 0
 
-### Xcode ###
+# Set dock icon size
+defaults write com.apple.dock tilesize -int 50
+
+# Show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent -bool true
+
+# Show status bar and path bar in Finder
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Search current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable warning when changing file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Disable warning when emptying trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Unhide ~/Library
+chflags nohidden ~/Library
+
+# Don't open Photos when a new device is plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
+
+### Xcode
 
 # Trim trailing whitespace
 defaults write com.apple.dt.Xcode DVTTextEditorTrimTrailingWhitespace -bool true
@@ -27,5 +55,6 @@ defaults write com.apple.dt.Xcode ShowDVTDebugMenu -bool true
 # Open quickly opens in the focused pane
 defaults write com.apple.dt.Xcode IDEEditorCoordinatorTarget_Click -string "FocusedEditor"
 
-# Don't open Photos when a new device is plugged in
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
+# Show build time in toolbar
+defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
+
