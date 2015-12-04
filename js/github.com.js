beef9932268e837c81a78936f38f482cd96f1e20
Scott Berrevoets <s.berrevoets@me.com>
2015-12-03 19:51:42 -0800

    Update :+1: & button script

--- js/github.com.js
+++ js/github.com.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-    if (document.URL.indexOf("lyft") >= 0) {
-        show_additional_comment_buttons();
+    if (document.URL.indexOf("lyft") < 0) {
+        return;
     }
+
+    show_additional_comment_buttons();
+
+    var observer = new WebKitMutationObserver(function(mutations, observer) {
+        var button = $('#thumb-and-label-submit');
+        if (button.length === 0) {
+            show_additional_comment_buttons();
+        }
+    });
+
+    observer.observe(document, {
+        subtree: true,
+        attributes: true
+    });
 })
 
 function create_button(title, handler) {
     var button = document.createElement('button');
     button.innerHTML = title;
     button.className = 'btn';
+    button.setAttribute('id', 'thumb-and-label-submit');
     button.setAttribute('type', 'submit');
     button.setAttribute('style', 'margin: 0 30px 0 0');
     button.addEventListener('click', handler);
@@ -15,21 +30,22 @@ function create_button(title, handler) {
 }
 
 function submit_comment_and_add_label(event) {
-    event.preventDefault();
     event.stopPropagation();
 
     // Enter and submit comment
     var commentForm = $('.js-new-comment-form');
     var textarea = commentForm.find('textarea');
-    textarea.val(":+1:");
-    commentForm.find('.btn-primary')[0].click();
+    var currentValue = textarea.val();
+    textarea.val(currentValue + ":+1:");
 
     // Add reviewer label
     $('.sidebar-labels').find('button')[0].click();
     setTimeout(function() {
         $('input[type="checkbox"][value="scott"]')[0].click();
-        $('body').click();
-    }, 200);
+        setTimeout(function() {
+            commentForm.find('.btn-primary')[0].click();
+        }, 200);
+    }, 500);
 }
 
 function insert_buttons(element) {
