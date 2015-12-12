5219b44548cd6998be9f9ca0447799e72a6e56b3
Scott Berrevoets <s.berrevoets@me.com>
2015-12-11 22:14:45 -0800

    Add Terminal configuration to dotfiles

--- osx/install.sh
+++ osx/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Bootstrap OS X configuration
 
+# Copy Terminal settings
+rm ~/Library/Preferences/com.apple.Terminal.plist
+ln -s ~/dotfiles/osx/Terminal.plist ~/Library/Preferences/com.apple.Terminal.plist
+
 if ! which brew &> /dev/null; then
   echo "Installing homebrew"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
