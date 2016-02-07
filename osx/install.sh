a1b114731cc6d930c74eb401f389f29e68e02435
Scott Berrevoets <s.berrevoets@me.com>
2016-02-06 23:11:23 -0800

    Remove unnecessary tap

--- osx/install.sh
+++ osx/install.sh
@@ -10,7 +10,6 @@ if ! which brew &> /dev/null; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew tap Homebrew/brewdler
 brew update
 brew bundle --file=Brewfile
 brew bundle --file=Caskfile
