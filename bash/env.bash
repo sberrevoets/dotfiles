bdfc3f1094004ff7a61ea285bc3b4385127a7877
Scott Berrevoets <s.berrevoets@me.com>
2016-04-07 23:24:35 -0700

    Move EDITOR and PATH to env.bash

---
+++ bash/env.bash
@@ -0,0 +1,8 @@
+# PATH:
+#  - /bin
+#  - /usr/sbin
+#  - /sbin
+#  - /usr/local/bin  (Homebrew)
+#  - /usr/bin        (system executables)
+export PATH="/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin"
+export EDITOR="$(which vim)"
