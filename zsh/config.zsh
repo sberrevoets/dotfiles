024faa1d438202c4558c1f612a77602455be8836
Scott Berrevoets <s.berrevoets@me.com>
2016-04-07 23:48:24 -0700

    Improve command completion

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,4 +1,15 @@
-# Use vim bindings in Terminal
+# Use vim in Terminal
 bindkey -v
+
+# Use Ctrl + R for history search
+bindkey '^R' history-incremental-search-backward
+
+# Complete commands starting with currently typed text
+autoload up-line-or-beginning-search
+autoload down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "\e[A" up-line-or-beginning-search
+bindkey "\e[B" down-line-or-beginning-search
 
 setopt AUTO_CD
