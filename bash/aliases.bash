40024d400f67fab0b94a5fc5f4f995d901c9965e
Scott Berrevoets <s.berrevoets@me.com>
2016-04-08 21:31:06 -0700

    Add speedtest alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -11,6 +11,7 @@ alias g=git
 
 alias df='cd ~/dotfiles'
 
+alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
 # Lyft
 alias instant='cd ~/Developer/instant'
 alias phoenix='cd ~/Developer/Lyft-iOS'
