a2b09d9b58b0e80c7748b676b2179e3a1b0d5c95
Scott Berrevoets <s.berrevoets@me.com>
2016-04-17 20:35:38 -0700

    Remove instant alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -13,6 +13,5 @@ alias df='cd ~/dotfiles'
 
 alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
 # Lyft
-alias instant='cd ~/Developer/instant'
 alias phoenix='cd ~/Developer/Lyft-iOS'
 
