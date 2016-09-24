c0e0b42bfac74a1da254c3aa1bb772ae4ce926a5
Scott Berrevoets <s.berrevoets@me.com>
2016-09-23 23:41:06 -0700

    Add zsh history settings

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,4 +39,32 @@ setopt ALWAYS_TO_END
 mkcd () { mkdir "$@" && cd "$@"; }
 
 # List directory contents on cd
+
+## History settings
+# Save x items to the given history file
+HISTSIZE=1000000000000000
+SAVEHIST=$HISTSIZE
+HISTFILE=$HOME/.zsh_history
+
+# Append history to the zsh_history file
+setopt APPEND_HISTORY
+
+# Write to history after each command
+setopt INC_APPEND_HISTORY
+
+# Don't store the history command
+setopt HIST_NO_STORE
+
+# Ignore duplicates in zsh history
+setopt HIST_IGNORE_ALL_DUPS
+
+# Ignore commands for history that start with a space
+setopt HIST_IGNORE_SPACE
+
+# Remove superfluous blanks from each line being added to the history list
+setopt HIST_REDUCE_BLANKS
+
+# After !! previous command don't execute, allow editing
+setopt HIST_VERIFY
+
 cd() { builtin cd "$@" && l; }
