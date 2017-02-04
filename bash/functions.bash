4225c513cf963926d77e755a05314b4644f689b5
Scott Berrevoets <s.berrevoets@me.com>
2017-02-03 20:14:16 -0800

    Add quick find function

--- bash/functions.bash
+++ bash/functions.bash
@@ -18,6 +18,7 @@ function gitignore() {
     curl -L -s https://www.gitignore.io/api/"$@" > .gitignore;
 }
 
+# Convenience function for pretty printing JSON
 function json() {
     if [ -t 0 ]; then
         # JSON as argument
@@ -26,4 +27,9 @@ function json() {
         # JSON from pipe
         python -mjson.tool | pygmentize -l javascript;
     fi;
+}
+
+# Quick find
+function f() {
+    find . -name "$@"
 }
