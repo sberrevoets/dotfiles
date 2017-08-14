54492ed8d3214c8467418ce154f783f4ec1291c8
Scott Berrevoets <s.berrevoets@me.com>
2017-08-14 00:18:56 -0700

    Show more useful sizes

--- macOS/functions.bash
+++ macOS/functions.bash
@@ -1,7 +1,7 @@
 # macOS specific `l`
 l() {
     if ls --color > /dev/null 2>&1; then
-        ls -lFg --color # GNU
+        ls -lFhg --color # GNU
     else
         ls -lFhG # Default macOS
     fi
