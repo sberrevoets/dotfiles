ee88bdac822fed00a4a83517ca647e3863f0f681
Scott Berrevoets <s.berrevoets@me.com>
2018-03-08 20:17:06 -0800

    Add alias for quick git root navigation

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -5,10 +5,10 @@ alias ....='cd ../../..'
 alias -- -='cd -'
 
 alias g=git
+# https://www.quora.com/How-can-I-make-a-git-alias-that-executes-a-cd
+alias gr='cd $(git rev-parse --show-toplevel)'
 alias v=vim
 
 alias df='cd ~/dotfiles'
 
 alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
-# Lyft
-alias phoenix='cd ~/Developer/Lyft-iOS'
