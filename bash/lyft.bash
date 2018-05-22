a0fa2e5e74ed5e6a69e537e4056ca22170be32e2
Scott Berrevoets <s.berrevoets@me.com>
2018-05-22 17:41:03 +0200

    Use -C for calling make from subdirectory

--- bash/lyft.bash
+++ bash/lyft.bash
@@ -1,8 +1,6 @@
 # Regenerates Lyft project from current directory
 function re() {
     export ALLOW_UNSUPPORTED_XCODE=1
-    gr
-    make update_dependencies
-    cd -
+    make -C $(git rev-parse --show-toplevel) update_dependencies
 }
 
