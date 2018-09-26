554bd9d35b75839c7fbeb1637ffa6e878e3d7dca
Scott Berrevoets <s.berrevoets@me.com>
2018-09-25 22:41:45 -0700

    Remove all JavaScript
    I wasn't using these anymore

--- js/atlassian.net.js
+++
@@ -1,5 +0,0 @@
-$(document).on('keydown', '.textarea', function(e) {
-    if (e.keyCode == 13 && (e.metaKey || e.ctrlKey)) {
-        $(this).parents('form').submit()
-    }
-})
--- js/lever.co.js
+++
@@ -1,5 +0,0 @@
-$(document).on('keydown', '.card-form textarea', function(e) {
-    if (e.keyCode == 13 && (e.metaKey || e.ctrlKey)) {
-        $(this).parents('form').find(':submit').click()
-    }
-})
