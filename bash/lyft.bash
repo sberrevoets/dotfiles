85ce544398370250e300215f0d50245d0300b63e
Scott Berrevoets <s.berrevoets@me.com>
2018-11-11 00:34:44 -0800

    Make default target Lyft only

--- bash/lyft.bash
+++ bash/lyft.bash
@@ -1,6 +1,5 @@
 # Regenerates Lyft project from current directory
 function re() {
     export ALLOW_UNSUPPORTED_XCODE=1
-    make -C $(git rev-parse --show-toplevel) update_dependencies
+    make -C $(git rev-parse --show-toplevel) update_dependencies targets=Lyft
 }
-
