25aa3dc575d0bf772fe9a6983d9485734fe3b767
Scott Berrevoets <s.berrevoets@me.com>
2020-02-10 12:20:01 -0800

    Remove unnecessary env variables

--- bash/lyft.bash
+++ bash/lyft.bash
@@ -1,7 +1,5 @@
 # Regenerates Lyft project from current directory
 function re() {
-    export ALLOW_UNSUPPORTED_XCODE=1
-    export FOCUS=1
     export INCLUDE_NON_APP_TRANSITIVE=2
     export NO_SWIFTLINT=1
     make -C $(git rev-parse --show-toplevel) update_dependencies targets=Lyft
