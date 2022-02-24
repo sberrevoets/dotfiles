730ce72a56ed316f7be462f84aea3a80bc6acd9a
Scott Berrevoets <s.berrevoets@me.com>
2022-02-23 17:23:05 -0800

    Install macOS preferences from main

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,11 @@ chsh -s $(which zsh)
 # Create base installation
 ./install.sh install
 
+# Install macOS preferences
+if [ "$(uname)" == "Darwin" ]; then
+    echo "macOS (Darwin) detected; sourcing macOS preferences"
+    ./macOS/install.sh
+fi
+
 # Install vim plugins and configurations
 ./vim/install.sh
