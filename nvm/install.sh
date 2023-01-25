45fdf22f6bf5b9f0394fd4db9c633d0f9c4de9d5
Scott Berrevoets <s.berrevoets@me.com>
2023-01-24 17:00:25 -0800

    [nvm] Add more default packages

--- nvm/install.sh
+++ nvm/install.sh
@@ -2,6 +2,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 nvm install node
 
-npm install -g typescript-language-server typescript # tsserver integration in neovim
+npm install -g typescript
+npm install -g typescript-language-server # tsserver integration in neovim
+npm install -g vscode-langservers-extracted # eslint in neovim
 npm install -g pyright # Python LSP integration
 npm install -g pdiffjson # Diff JSON with sorting options
+npm install -g eslint # ESLint
