1fae98c14461a04241693c0a2d97de72cdc86458
Scott Berrevoets <s.berrevoets@me.com>
2023-02-01 22:07:11 -0800

    [nvm] Bump installed version

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 nvm install node
 
