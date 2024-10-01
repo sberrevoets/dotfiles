cd5fad200af3dae21b65a5f89c6f6dde279ad3d7
Scott Berrevoets <s.berrevoets@me.com>
2024-09-30 23:59:05 -0700

    Add atuin
    After having enabled this for a while it's time to make this part of my
    workflow more.

---
+++ atuin/atuin.zsh
@@ -0,0 +1,11 @@
+export ATUIN_CONFIG_DIR="$DOTFILES/atuin"
+eval "$(atuin init zsh)"
+
+export ATUIN_NOBIND="true"
+eval "$(atuin init zsh)"
+
+bindkey '^r' atuin-search
+
+# bind to the up key, which depends on terminal mode
+bindkey '^[[A' atuin-up-search
+bindkey '^[OA' atuin-up-search
