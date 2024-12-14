588fd20fc302259a16542a89c80434e71db7fdd1
Scott Berrevoets <s.berrevoets@me.com>
2024-12-14 10:55:30 -0800

    Install bash-language-server by default

--- nvm/install.sh
+++ nvm/install.sh
@@ -6,5 +6,6 @@ npm install -g typescript
 npm install -g typescript-language-server # tsserver integration in neovim
 npm install -g vscode-langservers-extracted # eslint in neovim
 npm install -g pyright # Python LSP integration
+npm install -g bash-language-server # Bash LSP integration
 npm install -g pdiffjson # Diff JSON with sorting options
 npm install -g eslint # ESLint
