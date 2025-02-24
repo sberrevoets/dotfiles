1c75f0161e14d711b2e4afa513ec8afd014ed25b
Scott Berrevoets <s.berrevoets@me.com>
2025-02-23 16:09:18 -0800

    [nvim] Enable installing from any directory

--- vim/install.sh
+++ vim/install.sh
@@ -4,6 +4,6 @@ echo "Installing vim plugins"
 vim +PlugInstall +qall
 
 mkdir -p ~/.config/nvim/
-ln -s $PWD/vim/init.lua ~/.config/nvim/init.lua
+ln -s $DOTFILES/vim/init.lua ~/.config/nvim/init.lua
 
 python3 -m pip install --user --upgrade pynvim
