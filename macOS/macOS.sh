3e04d6233c0803c369257d493765f472d7ace3f1
Scott Berrevoets <s.berrevoets@me.com>
2025-05-11 21:20:46 -0700

    Delete unused env variables

--- macOS/macOS.sh
+++ macOS/macOS.sh
@@ -1,9 +1,5 @@
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-export ALLOW_UNSUPPORTED_XCODE=1
 export HOMEBREW_NO_ANALYTICS=1
-export NO_XCODE_LINT=1
-export COCOAPODS_DISABLE_STATS=1
 
 # https://github.com/Homebrew/homebrew-core/issues/15746
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
