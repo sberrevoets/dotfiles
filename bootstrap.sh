195ee934fc6138c09ae8fb4002b4b9cf58f64444
Scott Berrevoets <s.berrevoets@me.com>
2025-05-11 23:12:14 -0700

    Add ghostty config

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,10 @@ chsh -s $(which zsh)
 # Create base installation
 ./install.sh install
 
+# Configure ghostty
+mkdir -p ~/.config/ghostty
+ln -s $DOTFILES/ghostty  ~/.config/ghostty/config
+
 # Install macOS preferences
 if [ "$(uname)" == "Darwin" ]; then
     echo "macOS (Darwin) detected; sourcing macOS preferences"
