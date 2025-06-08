db65eabd178e45597d975448d92f428b3574fa1b
Scott Berrevoets <s.berrevoets@me.com>
2025-06-08 20:08:52 +0200

    [atuin] Initialize conditionally
    Ignore this if atuin isn't actually installed for whatever reason.

--- atuin/atuin.zsh
+++ atuin/atuin.zsh
@@ -1,11 +1,14 @@
-export ATUIN_CONFIG_DIR="$DOTFILES/atuin"
-eval "$(atuin init zsh)"
+if command -v atuin &> /dev/null; then
+  export ATUIN_CONFIG_DIR="$DOTFILES/atuin"
+  eval "$(atuin init zsh)"
 
-export ATUIN_NOBIND="true"
-eval "$(atuin init zsh)"
+  export ATUIN_NOBIND="true"
+  eval "$(atuin init zsh)"
 
-bindkey '^r' atuin-search
+  bindkey '^r' atuin-search
 
-# bind to the up key, which depends on terminal mode
-bindkey '^[[A' atuin-up-search
-bindkey '^[OA' atuin-up-search
+  # bind to the up key, which depends on terminal mode
+  bindkey '^[[A' atuin-up-search
+  bindkey '^[OA' atuin-up-search
+fi
+
