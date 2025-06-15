d127910c2bc504ea388243102c0da4ecc2779cd8
Scott Berrevoets <s.berrevoets@me.com>
2025-06-14 22:47:21 -0700

    [mas] Remove unused/deleted apps

--- macOS/mas.sh
+++ macOS/mas.sh
@@ -1,8 +1,6 @@
 mas install 1438243180 # Dark Reader for Safari
-mas install 1480933944 # Vimari
 mas install 1544743900 # Hush Nag Blocker
 mas install 1365531024 # 1Blocker: Ad Blocker & Privacy
-mas install 1523251484 # DrString
 mas install 1519867270 # Refined GitHub
 mas install 1458969831 # JSON Peep for Safari
 mas install 1376402589 # StopTheMadness
