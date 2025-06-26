7c0794c18ef546a1f3248be0c17ab77e9520858b
Scott Berrevoets <s.berrevoets@me.com>
2025-06-25 23:35:08 -0700

    [nvim] Change Telescope file finder shortcut
    This matches Xcode's cmd+shift+o so one less thing to remember.

--- vim/init.lua
+++ vim/init.lua
@@ -16,7 +16,7 @@ end, { desc = "Reload config" })
 local nvim_lsp = require("lspconfig")
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>fa", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>o", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Telescope find git files" })
 vim.keymap.set("n", "<leader>s", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
