dc50d613cb3b7e8b4dc65dd467dab8c485629d4d
Scott Berrevoets <s.berrevoets@me.com>
2026-02-03 15:45:34 -0800

    Add `./local/bin` to PATH

--- bash/env.bash
+++ bash/env.bash
@@ -1,5 +1,6 @@
 # PATH:
 #  - $HOME/.bin        (personalized)
+#  - $HOME/.local/bin  (personalized, certain tools install here)
 #  - /opt/homebrew/bin  (Homebrew Apple Silicon)
 #  - /bin
 #  - /usr/sbin
@@ -8,7 +9,7 @@
 #  - /usr/bin          (system executables)
 
 # TODO: Look into moving zshenv
-export PATH="$HOME/.bin:/opt/homebrew/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin"
+export PATH="$HOME/.bin:$HOME/.local/bin:/opt/homebrew/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin"
 export EDITOR="$(which nvim)"
 export CDPATH=".:$HOME/Developer"
 
