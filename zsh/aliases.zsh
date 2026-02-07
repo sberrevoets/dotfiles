d8dbfbee1242b983c31d8899a85247ab278ceb09
Scott Berrevoets <s.berrevoets@me.com>
2026-02-06 23:24:28 -0800

    [sh] Use bat to replace cat

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -20,5 +20,6 @@ alias d8='cd -8'
 alias d9='cd -9'
 alias d10='cd -10'
 
+alias cat='bat'
 alias ls='eza --icons --group-directories-first'
 alias ll='eza -la --git --icons --group-directories-first'
